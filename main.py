cd437105d2ed00c34d69059159ffd2b86c6a8bb2
Diego <dtellez95@gmail.com>
2022-01-13 15:52:01 +0000

    feat: recognition function implemenetd

--- main.py
+++ main.py
@@ -2,8 +2,18 @@ import speech_recognition as sr
 
 r = sr.Recognizer()
 
-with sr.Microphone() as source:
-    print("Hi, I am Diego's assistant bot, what do you need?")
-    audio = r.listen(source)
-    voice_data = r.recognize_google(audio)
-    print(voice_data)
+
+def recordAudio():
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+        try:
+            voiceData = r.recognize_google(audio)
+        except sr.UnknownValueError:
+            print('Sorry, I did not understand you my friend!')
+        except sr.RequestError:
+            print('Sorry I have a problem right now and I cant listen properly :(')
+        return voiceData
+
+
+print("Hi, I am Diego's assistant bot, what do you need?")
+voiceData = recordAudio()
