f59902a4fc0a31a2b4718d93b30f7906b7b80905
Diego <dtellez95@gmail.com>
2022-01-13 18:58:13 +0000

    feat: fix gitignore

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+from re import search
 import speech_recognition as sr
+import webbrowser
+from time import ctime
 
 r = sr.Recognizer()
 
@@ -6,10 +9,29 @@ r = sr.Recognizer()
 def respond(voiceData):
     if 'what is your name' in voiceData:
         print('My name is Delta')
+    if 'what time is it' in voiceData:
+        print(ctime())
+    if 'search' in voiceData:
+        search = recordAudio('What do you want to search for?')
+        url = 'https://google.com/search?q=' + search
+        webbrowser.get().open(url)
+        print('This is what I found for ' + search)
+    if 'find location' in voiceData:
+        location = recordAudio('Where do you want to go?')
+        url = 'https://google.nl/maps/place/' + location + '/&amp;'
+        webbrowser.get().open(url)
+        print('Enjoy the ride to ' + location)
+    if 'show me a video' in voiceData:
+        video = recordAudio('What do you want to watch?')
+        url = 'https://www.youtube.com/results?search_query=' + video
+        webbrowser.get().open(url)
+        print('Just relax and watch ' + video)
 
 
-def recordAudio():
+def recordAudio(ask=False):
     with sr.Microphone() as source:
+        if ask:
+            print(ask)
         audio = r.listen(source)
         try:
             voiceData = r.recognize_google(audio)
