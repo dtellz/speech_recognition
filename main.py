023c549e00937bb4fcf120992c58bb1344ca2c2b
Diego <dtellez95@gmail.com>
2022-01-13 20:55:00 +0000

    feat: improved hearing capabilities and wikipedia summaries

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from re import search
 import speech_recognition as sr
 import webbrowser
+import wikipedia
 import time
 import pyttsx3
 import datetime
 
 r = sr.Recognizer()
+r.energy_threshold = 300  # this is used to avoid noise
 
 
 class person:
@@ -38,27 +40,32 @@ def botSpeaks(text):
 
 # ANSWERS TO USER VOICE
 
+
 def respond(voiceData):
+
     if 'what is your name' in voiceData:
         botSpeaks('My name is' + bot.name)
-        print('My name is Delta')
     if 'what time is it' in voiceData:
-        print(datetime.datetime.now().strftime('%I:%M %p'))
+        botSpeaks(datetime.datetime.now().strftime('%I:%M %p'))
     if 'search in google' in voiceData:
         search = recordAudio('What do you want to search for?')
         url = 'https://google.com/search?q=' + search
+        botSpeaks('This is what I found for ' + search)
         webbrowser.get().open(url)
-        print('This is what I found for ' + search)
     if 'find location' in voiceData:
         location = recordAudio('Where do you want to go?')
         url = 'https://google.nl/maps/place/' + location + '/&amp;'
+        botSpeaks('Enjoy the ride to ' + location)
         webbrowser.get().open(url)
-        print('Enjoy the ride to ' + location)
     if 'show me a video' in voiceData:
         video = recordAudio('What do you want to watch?')
         url = 'https://www.youtube.com/results?search_query=' + video
+        botSpeaks('Just relax and enjoy your ' + video + 'videos')
         webbrowser.get().open(url)
-        print('Just relax and watch ' + video)
+    if 'tell me about' in voiceData:
+        subject = recordAudio('What do you want to know about?')
+        info = wikipedia.summary(subject, 1)
+        botSpeaks(info)
     # if 'let me hear all your possible voices' in voiceData:
         # checkVoices()
     if 'exit' in voiceData:
@@ -67,10 +74,11 @@ def respond(voiceData):
 
 
 def recordAudio(ask=False):
+
     voiceData = ''
     with sr.Microphone() as source:
         if ask:
-            print(ask)
+            botSpeaks(ask)
         audio = r.listen(source)
         try:
             voiceData = r.recognize_google(audio)
@@ -88,7 +96,7 @@ bot = assistant()
 bot.name = 'Delta'
 user.name = ''
 
-print("Hi, I am Diego's assistant bot, what do you need?")
+
 while 1:
     voiceData = recordAudio()
     respond(voiceData)
